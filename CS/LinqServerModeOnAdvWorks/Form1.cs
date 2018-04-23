ce4c172b3a75dd56852f3e368998a438b5045339
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/LinqServerModeOnAdvWorks/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraGrid.Columns;
+
+namespace LinqServerModeOnAdvWorks {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            gridControl1.ServerMode = true;
+
+            AdvWorksDataContext dc = new AdvWorksDataContext();
+            dc.Log = Console.Out;  // log queries
+
+            linqServerModeSource1.QueryableSource = dc.PurchaseOrderHeaders;
+            gridControl1.DataSource = linqServerModeSource1;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# LINQ to SQL Sever Mode Example
+
+
+<p>This is an example of using the XtraGrid in <a href="http://documentation.devexpress.com/#WindowsForms/CustomDocument4057">Server mode bound to LINQ to SQL classes</a>. It uses the LinqServerModeSource data source. The example fetches data from the AdventureWorks database on a local SQL Server. SQL queries are written to the Output window in Visual Studio IDE, so you can see what's going on behind the scene.</p>
+
+<br/>
+
+
